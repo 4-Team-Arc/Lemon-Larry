a8dfa2d3df8c4f7f06fba044027d0ac21791ec77
Antonio Ramirez <antonioramirez9614@gmail.com>
2024-08-26 10:31:02 -0500

    getting the game + site to load together

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,18 @@
 import NavBar from "./components/navbar.jsx"
 import LoginPage from "./components/loginPage.jsx"
 import AboutUsPage from "./components/about-us.jsx"
-import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
+import { Routes,Route } from "react-router-dom";
 
 const App = () => {
 
   return (
     <>
       <NavBar/>
-        <Router>
           <Routes>
             <Route exact path="/" element={<LoginPage/>}></Route>
             <Route exact path="/Login" element={<LoginPage/>}></Route>
             <Route exact path="/About-Us" element={<AboutUsPage/>}></Route>
           </Routes>
-        </Router>
     </>
   )
 }
